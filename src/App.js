ed30cd108c439cbbca5865935a5eda08566b3805
Malcolm_Leake <echelon1089@gmail.com>
2020-06-17 22:00:49 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   }
 
   render() {
-    // debugger;
+   
     return (
       <div className="App">
           <button onClick={() => this.handleOnClickItems()}>
@@ -33,9 +33,10 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  debugger;
-  return { items: state.items }
-}
 
-export default connect(mapStateToProps)(App);
+const vanilla = (milkshake) => {
+  debugger;
+  return { items: milkshake.items }
+}
+ 
+export default connect(vanilla)(App);
