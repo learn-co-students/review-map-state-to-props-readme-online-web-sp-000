48885346dc661b3d6950f9eea29eae6ef2843296
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-06-30 20:25:59 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={store}>;
     <App store={store}/>
   </Provider>,
   document.getElementById('root')
