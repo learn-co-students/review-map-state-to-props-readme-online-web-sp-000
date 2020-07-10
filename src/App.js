840f25d8f352d817033e74d333efa58f2d18236f
StephanieAlyssa <alyssa.yesilyurt@gmail.com>
2020-07-10 19:36:34 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -18,16 +18,16 @@ class App extends Component {
   }
 
   render() {
-    // debugger;
+    debugger;
     return (
       <div className="App">
-          <button onClick={() => this.handleOnClickItems()}>
-            Click to change items count
-            </button>
-          <button onClick={() => this.handleOnClickUsers()}>
-            Click to change user count
-          </button>
-          <p>{this.props.items.length}</p>
+        <button onClick={() => this.handleOnClickItems()}>
+          Click to change items count
+        </button>
+        <button onClick={() => this.handleOnClickUsers()}>
+          Click to change user count
+        </button>
+        <p> {this.props.items.length}</p>
       </div>
     );
   }
