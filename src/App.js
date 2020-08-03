a6219fc80acfcec9c71db2fd4566fbc624264183
Josh Ellis <joshe523@gmail.com>
2020-08-03 13:04:12 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,34 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './App.css';
-
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import './App.css'
 
 class App extends Component {
-
-  handleOnClickItems() {
+  handleOnClickItems () {
     this.props.store.dispatch({
-      type: 'GET_COUNT_OF_ITEMS',
-    });
+      type: 'GET_COUNT_OF_ITEMS'
+    })
   }
 
-  handleOnClickUsers() {
+  handleOnClickUsers () {
     this.props.store.dispatch({
-      type: 'GET_COUNT_OF_USERS',
-    });
+      type: 'GET_COUNT_OF_USERS'
+    })
   }
 
-  render() {
+  render () {
     // debugger;
     return (
-      <div className="App">
-          <button onClick={() => this.handleOnClickItems()}>
-            Click to change items count
-            </button>
-          <button onClick={() => this.handleOnClickUsers()}>
-            Click to change user count
-          </button>
-          <p>{this.props.items.length}</p>
+      <div className='App'>
+        <button onClick={() => this.handleOnClickItems()}>
+          Click to change items count
+        </button>
+        <button onClick={() => this.handleOnClickUsers()}>
+          Click to change user count
+        </button>
+        <p>{this.props.items.length}</p>
       </div>
-    );
+    )
   }
 }
 
-const mapStateToProps = (state) => {
-  debugger;
-  return { items: state.items }
-}
-
-export default connect(mapStateToProps)(App);
+export default connect(state => ({ items: state.items }))(App)
