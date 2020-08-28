9d6aa834666af9e74a0097440b5be16fdd90140a
Soner Mezgitci <mezgitci9@gmail.com>
2020-08-28 14:44:50 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -6,36 +6,38 @@ import './App.css';
 class App extends Component {
 
   handleOnClickItems() {
-    this.props.store.dispatch({
+    this.props.dispatch({
       type: 'GET_COUNT_OF_ITEMS',
     });
+    // {
+    //   type: 'GET_COUNT_OF_ITEMS',
+    // });
   }
 
   handleOnClickUsers() {
-    this.props.store.dispatch({
+    this.props.dispatch({
       type: 'GET_COUNT_OF_USERS',
     });
   }
 
+ 
   render() {
-    // debugger;
+    debugger;
     return (
       <div className="App">
-          <button onClick={() => this.handleOnClickItems()}>
-            Click to change items count
-            </button>
-          <button onClick={() => this.handleOnClickUsers()}>
-            Click to change user count
-          </button>
-          <p>{this.props.items.length}</p>
+        <button onClick={() => this.handleOnClickItems()}>
+          Click to change items count
+        </button>
+        <button onClick={() => this.handleOnClickUsers()}>
+          Click to change user count
+        </button>
+        <p> {this.props.ornagePeel}</p>
       </div>
     );
   }
 }
-
 const mapStateToProps = (state) => {
-  debugger;
-  return { items: state.items }
-}
-
+  return { orangePeel: ['a', 'b', 'c'] };
+};
+ 
 export default connect(mapStateToProps)(App);
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -6,11 +6,13 @@ export default function shoppingListItemReducer(state = {
   switch(action.type) {
 
     case 'GET_COUNT_OF_ITEMS':
+      console.log(" counting items")
       return Object.assign({}, state, {
         items: state.items.concat(state.items.length + 1)
       });
 
     case 'GET_COUNT_OF_USERS':
+        console.log(" counting Users")
       return Object.assign({}, state, {
         users: state.users.concat(state.users.length + 1)
       });
