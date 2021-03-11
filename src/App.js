627fdc94b3230b11e68f480acb9386aeebd1bc7b
Sarah Condon <s.j.condon01@gmail.com>
2021-03-10 21:14:15 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import './App.css';
 class App extends Component {
 
   handleOnClickItems() {
-    this.props.dispatch({
+    this.props.store.dispatch({
       type: 'GET_COUNT_OF_ITEMS',
     });
   }
 
   handleOnClickUsers() {
-    this.props.dispatch({
+    this.props.store.dispatch({
       type: 'GET_COUNT_OF_USERS',
     });
   }
@@ -34,7 +34,6 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  debugger;
   return { items: state.items }
 }
 
