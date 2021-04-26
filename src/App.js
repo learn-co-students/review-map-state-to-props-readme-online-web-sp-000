bd14d1cbfb59abd3c8c05ace252c49f6217198fa
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-26 15:16:17 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -34,8 +34,9 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  debugger;
+  
   return { items: state.items }
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(App)
+  // {export default connect(state => ({items: state.items}))(App)}
